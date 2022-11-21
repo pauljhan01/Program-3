f56c92cec01cb9121f2b697e8e7a5418fff4f345
Paul J. Han <pauljhan@utexas.edu>
2022-11-21 15:16:50 -0600

    no code

---
+++ Program3.java
@@ -0,0 +1,8 @@
+public class Program3 {
+
+    public int maxFoodCount (int[] plots) {
+        // Implement your dynamic programming algorithm here
+        // You may use helper functions as needed
+        return 0;
+    }
+}
